077360072fb4c0ad4280f222a04b676ac097a3fa
Bibek Shrestha <bibek@yipl.com.np>
2010-11-15 13:59:50 +0545

    added NIBL Epayment missing config in default config file

--- src/config.default.php
+++ src/config.default.php
@@ -9,3 +9,26 @@ $config['paymentGateway']['esewaEpay'] = array(
     'postUrl'           => 'http://localhost',
     'testPostUrl'       => 'http://localhost/test',
 );
+
+/**
+ * @todo refactor the default settings inside the class file
+ */
+$config['paymentGateway']['niblEpayment'] = array(
+    'BankId'        => '004',
+    'MD'            => "P",
+    'PID'           => '000000000082',
+    'ITC'           => 'NepaliMerchantApi',
+    'CRN'           => 'NPR',
+    'CG'            => 'N',
+    'USER_LANG_ID'  => '001',
+    'UserType'      => '1',
+    'AppType'       => 'retail',
+
+    // For Test Purpose
+    // 'RU'            => 'http://localhost',
+    // 'postUrl'       => 'http://202.63.245.70/BankAwayRetail/sgonHttpHandler.aspx?Action.ShoppingMall.Login.Init=Y?',
+
+    // For Real Purpose
+    'RU'            => 'http://localhost/',
+    'postUrl'       => 'http://202.63.245.70/BankAwayRetail/sgonHttpHandler.aspx?Action.ShoppingMall.Login.Init=Y?',
+);
